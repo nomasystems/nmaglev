887152938c688b12dfeb409f2837d7b21566b04c
tak30 <pablolvi@ext.zara.com>
2022-01-04 10:42:31 +0100

    feat: Add license and remove makefile

--- src/nmaglev.erl
+++ src/nmaglev.erl
@@ -1,19 +1,16 @@
-%%% Copyright (c) 2013 [Nomasystems, S.L. http://www.nomasystems.com]
-%%%
-%%% This file contains Original Code and/or Modifications of Original Code as
-%%% defined in and that are subject to the Nomasystems Public License version
-%%% 1.0 (the 'License'). You may not use this file except in compliance with
-%%% the License. BY USING THIS FILE YOU AGREE TO ALL TERMS AND CONDITIONS OF
-%%% THE LICENSE. A copy of the License is provided with the Original Code and
-%%% Modifications, and is also available at www.nomasystems.com/license.txt.
-%%%
-%%% The Original Code and all software distributed under the License are
-%%% distributed on an 'AS IS' basis, WITHOUT WARRANTY OF ANY KIND, EITHER
-%%% EXPRESS OR IMPLIED, AND NOMASYSTEMS AND ALL CONTRIBUTORS HEREBY DISCLAIM
-%%% ALL SUCH WARRANTIES, INCLUDING WITHOUT LIMITATION, ANY WARRANTIES OF
-%%% MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, QUIET ENJOYMENT OR
-%%% NON-INFRINGEMENT. Please see the License for the specific language
-%%% governing rights and limitations under the License.
+%%% Copyright 2021 Nomasystems, S.L. http://www.nomasystems.com
+%%
+%% Licensed under the Apache License, Version 2.0 (the "License");
+%% you may not use this file except in compliance with the License.
+%% You may obtain a copy of the License at
+%%
+%%     http://www.apache.org/licenses/LICENSE-2.0
+%%
+%% Unless required by applicable law or agreed to in writing, software
+%% distributed under the License is distributed on an "AS IS" BASIS,
+%% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+%% See the License for the specific language governing permissions and
+%% limitations under the License.
 %%%
 -module(nmaglev).
 
